860b91173abe31b30ecad49a985943c09c518a56
Gaurang Gupta <g12172001@gmail.com>
2026-02-28 21:22:23 +0530

    Added speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import Navbar from "@/app/components/layout/Navbar"
 import {createSupabaseServerClient} from "@/lib/supabase/server";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
     title: "IngestAI",
@@ -29,6 +30,7 @@ export default async function RootLayout({
                     {children}
                 </main>
                 <Analytics/>
+                <SpeedInsights/>
             </body>
         </html>
     )
